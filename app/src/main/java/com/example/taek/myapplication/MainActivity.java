c89746bfbd630d7e898f339894508afb93abff6b
blueguy28 <blueguy5982@gmail.com>
2017-11-29 23:02:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/taek/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.taek.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup() ;
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("TabSpec1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator("주택") ;
+        tabHost1.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("TabSpec2") ;
+        ts2.setContent(R.id.content1) ;
+        ts2.setIndicator("토지") ;
+        tabHost1.addTab(ts2) ;
+
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("TabSpec3") ;
+        ts3.setContent(R.id.content1) ;
+        ts3.setIndicator("기타") ;
+        tabHost1.addTab(ts3) ;
+    }
+}
